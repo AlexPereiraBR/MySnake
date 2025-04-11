d181d0920de12c0dbfa01cf2e3a82292e2ff0a56
AlexPereiraBR <aleksandrshchukinbr@icloud.com>
2025-04-10 23:47:32 -0300

    first commit

--- Snake/GameViewController.swift
+++ Snake/GameViewController.swift
@@ -14,32 +14,18 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+        let scene = GameScene(size: view.bounds.size)
+        
+        guard let skView = view as? SKView else {
+            return
         }
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+        
     }
 }
